86646f7fe99061af325a01db87e89362a683fc48
yeongeunim <yeongeunim322@gmail.com>
2024-10-18 13:13:06 +0900

    조건문

--- src/conditional/Multiplication.java
+++ src/conditional/Multiplication.java
@@ -1,27 +1,61 @@
 package conditional;
 
+import java.util.Scanner;
+
 public class Multiplication {
+    static void gugudan() {
+        for (int dan = 2; dan <= 9; dan++) {
+            for (int num = 1; num <= 9; num++) {
+                System.out.println(dan + "*" + num + "=" + num * dan);
+            }
+            System.out.println();
+        }
+    }
+
+    static void aToZ() {
+        for (char startChar = 'a'; startChar <= 'z'; startChar++) {
+            System.out.print(startChar);
+        }
+        System.out.println();
+    }
+
+    static void matrix() {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 32; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         // 사용자로부터 정수를 입력
-        int inputValue = 1;
+        int inputValue = 3;
 
         // if series
         // 1 이면 구구단 출력
         if (inputValue == 1) {
-            for(int dan = 2 ; dan <= 9 ; dan++) {
-                for(int num = 1 ; num <= 9 ; num++) {
-                    System.out.print(dan + "*" + num + " ");
-                }
-                System.out.println();
-            }
+            gugudan();
+            // 2 이면 a ~ z 출력
         } else if (inputValue == 2) {
-            for (char startChar = 'A'; startChar <= 'Z'; startChar++) {}
-
+            aToZ();
+            // 3 이면 8 X 32 "*" 출력
         } else if (inputValue == 3) {
-
+            matrix();
         }
-        // 2 이면 a ~ z 출력
-        // 3 이면 8 X 32 "*" 출력
+
+        // switch
+        switch (inputValue) {
+            case 1:
+                gugudan();
+                break;
+            case 2:
+                aToZ();
+                break;
+            case 3:
+                matrix();
+                break;
+        }
     }
 }
