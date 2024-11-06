df40224b9d8ffc83afa5cc3a41b1984e5657407d
yeongeunim <yeongeunim322@gmail.com>
2024-11-06 21:11:10 +0900

    반복문

---
+++ src/loop/DoWhile1.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class DoWhile1 {
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            System.out.print("i의 값: " + i);
+            i++;
+        } while (i < 3);
+    }
+}
---
+++ src/loop/LoopExample1.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class LoopExample1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i+ " ");
+            i++;
+        }
+    }
+}
