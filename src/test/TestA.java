7573cac76fde4773327a62e2b6a3c4eb952b1cc8
yeongeunim <yeongeunim322@gmail.com>
2024-11-28 21:53:23 +0900

    Test

---
+++ src/test/TestA.java
@@ -0,0 +1,14 @@
+package test;
+
+public class TestA {
+    public static void main (String[]args) {
+
+    // float[] noiseList; // 단순하게 생각해라! 그냥 변수 선언
+    // 근데 Primitive 변수가 아니고...Reference 변수
+    // 즉 Reference 변수는 에모리 주소 값을 저장한다.
+    // 근데..현재 저장된 메모리 주소 값이 없다.
+
+    // noiseList[2] = 2. 0f;
+    // noiseList 변수에 저장된 메모리 주소로 이동 // 근데.........변수에 저장된 메모리 소 값이 NULL 이다.
+    }
+}
