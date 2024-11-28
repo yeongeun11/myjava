3309dac4dd7cb7ea9b19e1e5e29de1283f1cda69
yeongeunim <yeongeunim322@gmail.com>
2024-11-28 21:53:30 +0900

    Test

---
+++ src/test/TestB.java
@@ -0,0 +1,15 @@
+package test;
+
+public class TestB {
+    public static void main(String[] args) {
+        float[][] noiseMatrix;
+
+
+        // 2초마다 평균 값을 계산해서 출력하는데
+        // 2초마다 측정 된 값을 1 frame 이라고 가정하고,
+        // 현재까지 측정괸 모든 프레임의 값들을 저장하고 싶음.
+
+        // 리터럴 상수로 하면 감점 그냥 상수로
+        // Java에서 상수를 생성할 땐 무조건 대문자
+    }
+}
