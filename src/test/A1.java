4479e4e9289a74fb11b959d22d3a5142d0317958
yeongeunim <yeongeunim322@gmail.com>
2024-12-03 13:33:26 +0900

    Test

---
+++ src/test/A1.java
@@ -0,0 +1,12 @@
+package test;
+
+public class A1 {
+
+    // 메서드(Method)
+    static int getSum(int argA, int argB){
+        return argA+argB;
+    }
+    public static void main(String[] args) {
+
+    }
+}
