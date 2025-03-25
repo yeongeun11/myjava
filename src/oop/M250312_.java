56042af1fa49c49423e83a384861453a07ce4ed9
yeongeunim <yeongeunim322@gmail.com>
2025-03-25 22:04:36 +0900

    oop

--- src/oop/M250312_.java
+++ src/oop/M250312_.java
@@ -4,6 +4,7 @@ class bar {
     // Overloading
 }
 
+class foo {}
 public class M250312_ {
     public static void main(String[] args) {
         bar b1 = new bar();
---
+++ src/oop/MainTest1.java
@@ -0,0 +1,25 @@
+package oop;
+/*
+1. 클래스와 인스턴스의 차이점을 설명하시오.
+        - Java 코드 예시를 포함하세요.
+        - 클래와 인스턴의 정의, 메모리 상의 차이, 용도 차이 등을 포함하세요.
+ */
+
+class Bar1 {
+
+}
+class Foo1 {
+
+}
+
+public class MainTest1 {
+    public static void main(String[] args) {
+
+        // new Bar -> Bar 클래스 명세서를 가지고 객체를 생성한다.
+        // 객체와 인스턴스의 차이점
+        Bar bar1 = new Bar();
+        Bar bar2 = new Bar();
+        Foo1 foo = new Foo1();
+
+    }
+}
