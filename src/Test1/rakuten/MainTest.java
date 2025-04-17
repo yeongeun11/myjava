45517062b4dcd57b2bc97ca6481cb789d555effa
yeongeunim <yeongeunim322@gmail.com>
2025-04-17 23:49:18 +0900

    oop

---
+++ src/Test1/rakuten/MainTest.java
@@ -0,0 +1,10 @@
+package Test1.rakuten;
+
+import Test1.gsc.GscStudent;
+
+public class MainTest {
+    public static void main(String[] args) {
+        GscStudent userInfo = new GscStudent();
+        System.out.println(userInfo.getX());//
+    }
+}
