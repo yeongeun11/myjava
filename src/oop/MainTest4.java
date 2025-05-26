a3964a9007e0130a3d1b9136d5be37252495440e
yeongeunim <yeongeunim322@gmail.com>
2025-05-26 15:42:06 +0900

    oop

---
+++ src/oop/MainTest4.java
@@ -0,0 +1,11 @@
+//package oop;
+//
+//
+//public class MainTest4 {
+//    public static void main(String[] args) {
+//
+//        Exception exception = new Exception();
+//
+//        throw  exception;
+//    }
+//}
