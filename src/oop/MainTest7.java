70b38564b1724c0f251bb92a2b96f709651e8fd0
yeongeun11 <166793365+yeongeun11@users.noreply.github.com>
2025-07-14 21:20:37 +0900

    oop

---
+++ src/oop/MainTest7.java
@@ -0,0 +1,18 @@
+package oop;
+
+class Person1{
+    String name;
+    int age;
+    Person1(String name, int age){this.name=name; this.age=age;}
+}
+
+public class MainTest7 {
+    public static void main(String[] args) {
+
+        Integer myValue = 10;
+        Float myFloat = 5.0f;
+        Double myDouble = 5.0;
+        Byte myByte = Byte.valueOf((byte)1);
+        Short myShort = Short.valueOf((short)1);
+    }
+}
